644f71f3c4513afb9d0a5e8b1663fd9b18c71895
André Nishitani <atoshio25@gmail.com>
2017-12-06 15:11:28 -0200

    Updates default image to xenial and includes Qt 5.9.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-UBUNTU_DISTRO=("trusty")
-QT_VERSION=("487" "571")
+BASE_IMAGE=("ubuntu:xenial")
+QT_VERSION=("487" "571" "593")
 
 for VERSION in "${QT_VERSION[@]}"
 do
   FOLDER="${VERSION:0:1}.${VERSION:1:1}.${VERSION:2:1}"
   mkdir -p $FOLDER
   cat Dockerfile.template \
-    | sed "s/\%UBUNTU_DISTRO\%/$UBUNTU_DISTRO/" \
+    | sed "s/\%BASE_IMAGE\%/$BASE_IMAGE/" \
     | sed "s/\%QT_VERSION_MAJOR\%/${VERSION:0:1}/" \
     | sed "s/\%QT_VERSION_MINOR\%/${VERSION:1:1}/" \
     | sed "s/\%QT_VERSION_BUILD\%/${VERSION:2:1}/" > $FOLDER/Dockerfile
+  echo "Qt v$VERSION [ UPDATED ]"
 done
