0f8c7be8219c30f30942e4788983ecb61a654c5c
André Nishitani <atoshio25@gmail.com>
2017-12-07 14:35:40 -0200

    Fix bootstrap script and Dockerfile and adds CircleCI file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,24 @@ QT_VERSION=("486" "571" "580" "593")
 
 for VERSION in "${QT_VERSION[@]}"
 do
-  FOLDER="${VERSION:0:1}.${VERSION:1:1}.${VERSION:2:1}"
-  mkdir -p $FOLDER
-  cat Dockerfile.template \
-    | sed "s/\%BASE_IMAGE\%/$BASE_IMAGE/" \
-    | sed "s/\%QT_VERSION_MAJOR\%/${VERSION:0:1}/" \
-    | sed "s/\%QT_VERSION_MINOR\%/${VERSION:1:1}/" \
-    | sed "s/\%QT_VERSION_BUILD\%/${VERSION:2:1}/" > $FOLDER/Dockerfile
-  echo "Qt v$VERSION [ UPDATED ]"
+    QT_FILE='qt-everywhere-opensource-src-${QT_VERSION}'
+    QT_URL='http://download.qt.io/archive/qt/${QT_MAJOR}.${QT_MINOR}/${QT_VERSION}'
+    FOLDER="${VERSION:0:1}.${VERSION:1:1}.${VERSION:2:1}"
+    mkdir -p $FOLDER
+    if [ ${VERSION:0:1} -gt 4 ]
+    then
+        QT_FILE=${QT_FILE}".tar.xz"
+        QT_URL=$QT_URL'/single/$QT_FILE'
+    else
+        QT_FILE=$QT_FILE".tar.gz"
+        QT_URL=$QT_URL'/$QT_FILE'
+    fi
+    cat Dockerfile.template \
+    | sed -e "s@\%BASE_IMAGE\%@$BASE_IMAGE@" \
+    | sed -e "s@\%QT_VERSION_MAJOR\%@${VERSION:0:1}@" \
+    | sed -e "s@\%QT_VERSION_MINOR\%@${VERSION:1:1}@" \
+    | sed -e "s@\%QT_VERSION_BUILD\%@${VERSION:2:1}@" \
+    | sed -e "s@\%QT_FILE\%@$QT_FILE@" \
+    | sed -e "s@\%QT_URL\%@$QT_URL@" > $FOLDER/Dockerfile
+    echo "Qt v$VERSION [ UPDATED ]"
 done
